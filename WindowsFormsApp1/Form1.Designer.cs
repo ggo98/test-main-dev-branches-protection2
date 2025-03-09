6ccd7a48b20d5b3db1a60c02bf33199df63fcf33
ggo98 <ggo98@yahoo.com>
2025-03-09 07:49:20 -0700

    master5 bis (#13)
    * Main (#8)
    
    * move 2
    
    * feature/add-button1 (#3)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * Release/master2 (#5)
    
    * move 2 (#1)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * feature/add-button1 (#2)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * feature/add-button2
    
    ---------
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * Feature/add button2 (#6)
    
    * move 2 (#1)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * feature/add-button1 (#2)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * feature/add-button2
    
    ---------
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    ---------
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * feature/add-button4 (#9)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    * feature/add-button5 (#11)
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>
    
    ---------
    
    Co-authored-by: Olivier gg <ggo98@outlook.com>

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -79,11 +80,23 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(69, 197);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 32);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 623);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -105,6 +118,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
